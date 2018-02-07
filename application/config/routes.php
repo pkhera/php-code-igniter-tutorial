ac768f7d437543d3daa0226c66e359d580f56379
Piyush Khera <piyush.khera88@gmail.com>
2018-02-08 01:43:19 +0530

    news section

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,7 +49,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
+$route['news/(:any)'] = 'news/view/$1';
+$route['news'] = 'news';
 $route['default_controller'] = 'pages/view';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
 $route['(:any)'] = 'pages/view/$1';
+
